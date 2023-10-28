52744ff3b09428b0d0b6e90d886cc1f968644b90
Zach <Zach@LAPTOP-1PSRNS9S>
2023-10-28 06:08:09 -0500

    Add project files.

---
+++ Stronk/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TLDLoader;
+namespace Stronk
+{
+    public class Stronk : Mod
+    {
+
+        public override string ID
+        {
+            get
+            {
+                return "Stronk";
+            }
+        }
+
+
+        public override string Name
+        {
+            get
+            {
+                return "Stronk";
+            }
+        }
+
+
+        public override string Author
+        {
+            get
+            {
+                return "Zach.O";
+            }
+        }
+
+
+        public override string Version
+        {
+            get
+            {
+                return "0.0.0";
+            }
+        }
+
+
+        public override void OnLoad()
+        {
+        }
+
+        public override void Update()
+        {
+            mainscript.M.player.maxWeight = 2147483647f;
+            mainscript.M.player.maxPickupForce = 2147483647f;
+        }
+       
+
+
+    }
+}
