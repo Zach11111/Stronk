560db1b4e4c731d6f51cf40c49822086b11128d5
Zach O <zachcatlover23@outlook.com>
2023-11-02 05:41:07 -0500

    remove debug stuff

--- Stronk/Stronk.cs
+++ Stronk/Stronk.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using TLDLoader;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Stronk
@@ -42,7 +43,7 @@ namespace Stronk
         {
             get
             {
-                return "1.1.0";
+                return "1.1.1";
             }
         }
 
@@ -63,38 +64,13 @@ namespace Stronk
             mainscript.M.player.maxPickupForce = maxPickupForceSliderValue;
             mainscript.M.player.throwForceM = throwForceSliderValue;
             mainscript.M.player.throwForceGain = throwForceGainSliderValue;
-          
- 
-         }
-        /* debug stuff
-        public override void OnGUI()
-        {
-            GUI.Label(new Rect(
-                75, 100, // offset from the top-left of the screen
-                1000, 700), // maximum pixel size of the box
-                string.Format("<color=orange><size=25>gain: {0}</size></color>", // format string
-                mainscript.M.player.throwForceGain // format arguments
-            ));
-            GUI.Label(new Rect(
-                75, 130, // offset from the top-left of the screen
-                1000, 700), // maximum pixel size of the box
-                string.Format("<color=orange><size=25>weight: {0}</size></color>", // format string
-                mainscript.M.player.maxWeight // format arguments
-            ));
-            GUI.Label(new Rect(
-                75, 160, // offset from the top-left of the screen
-                1000, 700), // maximum pixel size of the box
-                string.Format("<color=orange><size=25>force: {0}</size></color>", // format string
-                mainscript.M.player.maxPickupForce // format arguments
-            ));
-            GUI.Label(new Rect(
-                75, 190, // offset from the top-left of the screen
-                1000, 700), // maximum pixel size of the box
-                string.Format("<color=orange><size=25>throw force: {0}</size></color>", // format string
-                mainscript.M.player.throwForceM // format arguments
-            ));
+           
+
+
         }
-        */
+       
+       
+        
 
 
 
